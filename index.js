25b9a3f15183dd44dc4e535d4a1171d051179daa
Parjanya Modi <71305818+parjanyamodi@users.noreply.github.com>
2022-10-04 23:12:59 +0530

    ejs setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
