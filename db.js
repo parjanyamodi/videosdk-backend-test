bfe9e981a98a27300a9a25f0e2d43f1a1bafeb55
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2022-10-06 07:27:29 +0530

    Mongo link update

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
 const uri = "mongodb://127.0.0.1:27017/videosdk-test";
-mongoose.connect(uri).then(() => {
-  console.log("DataBase connection successful");
-});
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DataBase connection successful");
+  });
