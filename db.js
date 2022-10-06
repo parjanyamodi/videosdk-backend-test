64cc52b322509a323fb1fc8326fb239befa299bd
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2022-10-06 07:23:35 +0530

    Mongo link update

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const uri =
-  "mongodb+srv://admin:toor2022@127.0.0.1:27017/videosdk-test?retryWrites=true&w=majority";
+  "mongodb://admin:toor2022@127.0.0.1:27017/videosdk-test?retryWrites=true&w=majority";
 mongoose.connect(uri).then(() => {
   console.log("DataBase connection successful");
 });
