ca1b8bc9d46c59fea8b23312199b6913745ee6cb
Parjanya Modi <71305818+parjanyamodi@users.noreply.github.com>
2022-11-17 16:39:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -100,4 +100,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(8080, () => console.log("Server is running on port 8080"));
+server.listen(5000, () => console.log("Server is running on port 5000"));
